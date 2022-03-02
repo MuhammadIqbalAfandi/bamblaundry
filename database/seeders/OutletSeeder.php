29a00fa26718e66a7219f107adb41cd8f03c624e
Muhammad Iqbal Afandi <muhammadiqbalafandi30@gmail.com>
2022-03-02 15:42:27 +0700

    fix: outlet seeder

--- database/seeders/OutletSeeder.php
+++ database/seeders/OutletSeeder.php
@@ -15,15 +15,8 @@ class OutletSeeder extends Seeder
     public function run()
     {
         Outlet::create([
-            'outlet_number' => 'OT' . now()->format('YmdHms'),
+            'outlet_number' => 'OT' . now()->format('YmdHis'),
             'name' => 'Semua Outlet',
-            'address' => '',
-            'phone' => '',
-        ]);
-
-        Outlet::create([
-            'outlet_number' => 'OT' . now()->format('YmdHms'),
-            'name' => 'Outlet 1',
             'address' => '',
             'phone' => '',
         ]);
